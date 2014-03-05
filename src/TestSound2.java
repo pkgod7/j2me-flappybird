b1a5e4db409e8a1118795f645846f63b7118442c
Choong Peng Ken <pkgod7@gmail.com>
2014-03-06 00:41:07 +0800

    ok

--- src/TestSound2.java
+++ src/TestSound2.java
@@ -2,19 +2,26 @@ import javax.microedition.media.*;
 public class TestSound2 extends Thread
 {   
     public String x1;
+    public Player p1;
     public TestSound2(String x)
     {
         x1=x;
+    }
+    public void run()
+    {
         try
         {
-            Manager.createPlayer(getClass().getResourceAsStream(x1+".wav"),"audio/x-wav").start();
-            Thread.sleep(50);
+            p1=Manager.createPlayer(getClass().getResourceAsStream(x1+".wav"),"audio/x-wav");
+            p1.start();
+            Thread.sleep(550);
         }
         catch(Exception err)
         {
         }
-    }
-    public void run()
-    {
+        finally
+        {
+            p1.deallocate();
+            p1.close();
+        }
     }
 }
